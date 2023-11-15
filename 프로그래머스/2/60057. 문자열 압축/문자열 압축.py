35ad1e748e99690db8ce97671aa05a643a37adfb
Soobin <48948604+bonasoobin@users.noreply.github.com>
2023-11-15 21:46:15 +0900

    [level 2] Title: 문자열 압축, Time: 0.06 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.py
@@ -25,30 +25,37 @@ def solution(s):
         count = 1
         e = []
         for j in range(len(c[i])):
+            # 마지막에 범위오류 생기지 않도록 if문 지정
             if j < (len(c[i])-1):
+                # 만약 지금꺼랑 다음꺼랑 같다면 count += 1
                 if c[i][j] == c[i][j+1]:
                     count += 1
                 else:
+                    # 지금꺼랑 다음꺼랑 같지 않은데 j=0이 아닐때
                     if (j-1) >= 0:
                         f = []
-                        
+                        # count가 1이라면 count = 1.로 지정해주기(나중에 1 없앨건데 10의 1이 지워지지않게)
                         if count == 1:
                             count = 1.
                             f.append(str(count))
                             f.append(str(c[i][j]))
+                        # count가 1이 아니라면 그대로
                         else:
                             f.append(str(count))
                             f.append(str(c[i][j-1]))
                         count = 1
                         result = ''.join(f)
                         e.append(result)
+                    # 지금꺼랑 다음꺼랑 같지 않은데 j=0일때 (count=1일수밖에없음(처음이니까) -> 그냥 지금꺼 추가해주기)
                     else:
                         f = []
                         f.append(str(c[i][j]))
                         result = ''.join(f)
                         e.append(result)                    
+            # 마지막꺼일때
             else:
                 f = []
+                # 위와 같은 방식으로 count=1이면 1.로 바꿔주고 아니라면 그대로 추가해주기
                 if count == 1:
                     count = 1.
                     f.append(str(count))
@@ -61,9 +68,13 @@ def solution(s):
                     result = ''.join(f)
                     e.append(result)
         d.append(e)
+    '''    
+    print(d)
+    [['2a', '2b', '1.0a', '3c'], ['aa', '1.0bb', '1.0ac', '1.0cc'], ['aab', '1.0bac', '1.0cc'], 
+    ['aabb', '1.0accc'], ['aabba', '1.0ccc'], ['aabbac', '1.0cc'], ['aabbacc', '1.0c'], ['1.0aabbaccc']]
+    '''
     
-    
-    
+    # 1.0이 있다면 제외해주기
     h = []
     for i in d:
         g = []
@@ -71,10 +82,19 @@ def solution(s):
         result = ''.join(g)
         result = result.replace('1.0', '')
         h.append(result)
-    
-    
+    '''
+    print(h)
+    ['2a2ba3c', 'aabbaccc', 'aabbaccc', 'aabbaccc', 'aabbaccc', 'aabbaccc', 'aabbaccc', 'aabbaccc']
+    '''
+
+    # 길이 구하기
     q = []
     for i in range(len(h)):
         q.append(len(h[i]))
+    '''
+    print(q)
+    [7, 8, 8, 8, 8, 8, 8, 8]
+    '''
 
+    # 최솟값 return
     return min(q)
