13a00e077cdfb2d38be98a9a6f851973f40838da
Soobin <48948604+bonasoobin@users.noreply.github.com>
2024-06-03 21:55:46 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.py
@@ -1,16 +1,3 @@
 def solution(jobs):
-    a = 0
-    t = 0
-    length = len(jobs)
-    jobs.sort(key = lambda x: x[1])
-    
-    while len(jobs) > 0:
-        for i in jobs:
-            if i[0] <= a:
-                jobs.remove(i)
-                a += i[1]-1
-                t += a - i[0] +1
-                break
-        a+=1
-    return t//length 
-
+    answer = 0
+    return answer
