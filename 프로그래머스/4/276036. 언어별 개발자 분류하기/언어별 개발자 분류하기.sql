bfd4e8895e68d94db0d6db42b29823929d538bed
Soobin <48948604+bonasoobin@users.noreply.github.com>
2025-03-04 20:14:04 +0900

    [level 4] Title: 언어별 개발자 분류하기, Time: , Memory: undefined -BaekjoonHub

--- 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
+++ 프로그래머스/4/276036. 언어별 개발자 분류하기/언어별 개발자 분류하기.sql
@@ -1,53 +1,24 @@
-WITH 
-    SKILLS AS (
-        SELECT a.ID, b.NAME, b.CATEGORY
-        FROM 
-            DEVELOPERS a, SKILLCODES b
-            WHERE a.SKILL_CODE & b.CODE > 0
-    ),
-    FRONTEND_DEV AS (
-        SELECT DISTINCT ID
-        FROM SKILLS
-        WHERE CATEGORY = "Front End"
-    ),
-    PYTHON_DEV AS (
-        SELECT DISTINCT ID
-        FROM SKILLS
-        WHERE NAME = "Python"
-    ),
-    GRADE_A AS (
-        SELECT DISTINCT a.ID
-        FROM FRONTEND_DEV a, PYTHON_DEV b
-        WHERE a.ID = b.ID
-    ),
-    GRADE_B AS (
-        SELECT DISTINCT ID
-        FROM SKILLS
-        WHERE 
-            NAME = "C#" 
-            AND ID NOT IN (SELECT * FROM GRADE_A)
-    ),
-    GRADE_C AS (
-        SELECT ID
-        FROM FRONTEND_DEV
-        WHERE ID NOT IN (
-            SELECT ID FROM GRADE_A 
-            UNION
-            SELECT ID FROM GRADE_B
-        )
-    ),
-    ABC_DEVELOPERS AS (
-        SELECT "A" AS GRADE, a.ID
-        FROM GRADE_A a
-        UNION ALL
-        SELECT "B" AS GRADE, b.ID
-        FROM GRADE_B b
-        UNION ALL
-        SELECT "C" AS GRADE, c.ID
-        FROM GRADE_C c
-    )
-    
-SELECT a.GRADE, a.ID, b.EMAIL
-FROM ABC_DEVELOPERS a, DEVELOPERS b
-WHERE a.ID = b.ID
-ORDER BY a.GRADE, a.ID;
+WITH GROUP_DEVELOPERS AS (
+    SELECT 
+        ID, 
+        EMAIL, 
+        GROUP_CONCAT(DISTINCT NAME, CATEGORY) AS GROUPED
+    FROM 
+        SKILLCODES
+    JOIN 
+        DEVELOPERS
+        ON DEVELOPERS.SKILL_CODE & SKILLCODES.CODE
+    GROUP BY 1,2
+)
+SELECT 
+    CASE
+        WHEN GROUPED LIKE '%Front%' AND GROUPED LIKE '%Python%' THEN 'A'
+        WHEN GROUPED LIKE '%C#%' THEN 'B' 
+        WHEN GROUPED LIKE '%Front%' THEN 'C'
+    END AS GRADE,
+    ID, 
+    EMAIL
+FROM
+    GROUP_DEVELOPERS
+HAVING GRADE IS NOT NULL
+ORDER BY GRADE, ID
